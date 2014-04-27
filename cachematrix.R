5b74edca1d84f7af0018d2da90bb6a619cacccd7
AlgoCoder <coderalgo@gmail.com>
2014-04-27 16:45:00 -0400

    Update cachematrix.R
    Commit by AlgoCoder on April 27, 2014 4:44PM EDT

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions outlined below - makceCacheMatrix and cacheSolve
+## are used to create a wrapper around a numeric matrix. The wrapper 
+## provides accessor methods to get / set the values of the matrix 
+## and its inverse, so that is possible to cache the inverse.
 
-## Write a short comment describing this function
+## This functions provides a list of accessor methods on the input
+## matrix. Pass an invertible numeric matrix to this function whose
+## inverse can be cached
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+      xinv <- NULL
+      set <- function(y)  {
+        
+        x <<- y
+        xinv <<- NULL
+      }
+      
+      get <- function() x
+      
+      setinv <- function(minv) xinv <<- minv
+      
+      getinv <- function() xinv
+      
+      list(set = set, get = get, setinv = setinv, getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function calls makeCacheMatrix to see if the inverted 
+## matrix is already cached; if not, it computes the inverse 
+## using 'solve' and invokes the setter to store the inverse
+## Pass a wrapper list output from makeCacheMatrix as input to
+## this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        xinv <- x$getinv()
+        if(!is.null(xinv))  {
+          
+          message("getting cached data")
+          return(xinv)
+        }
+        data <- x$get()
+        xinv <- solve(data, ...)
+        x$setinv(xinv)
+        xinv
 }
